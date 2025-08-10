739ebbbe137f5f975a2a46b3c78f03bead6e5f81
AmulyaKashyap <ammu2734@gmail.com>
2025-08-10 17:55:19 +0530

    backend frontend separation

---
+++ backend/src/main/java/com/kashmau/track_fitness/TrackFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.kashmau.track_fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackFitnessApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kashmau/track_fitness/TrackFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashmau.track_fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
